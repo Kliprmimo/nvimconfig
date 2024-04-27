fe2a08a9fa978b0921f755829ba3e7217406c7ab
Kliprmimo <grzes.dziechciarz@gmail.com>
2024-04-27 20:11:13 +0200

    added autoclosing and autocomplete

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,20 +1,21 @@
 return {
-        {
-                'nvim-telescope/telescope.nvim', tag = '0.1.6',
-                -- or                            , branch = '0.1.x',
-                dependencies = { {'nvim-lua/plenary.nvim'} }
-        },
-        {
-                'rose-pine/neovim',
-                as = 'rose-pine',
-                config = function()
-                        vim.cmd('colorscheme rose-pine')
-                end
-        },
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-treesitter/playground',
-        'theprimeagen/harpoon',
-        'mbbill/undotree',
-        'tpope/vim-fugitive'
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		dependencies = { { 'nvim-lua/plenary.nvim' } }
+	},
+	{
+		'rose-pine/neovim',
+		as = 'rose-pine',
+		config = function()
+			vim.cmd('colorscheme rose-pine')
+		end
+	},
+	'nvim-treesitter/nvim-treesitter',
+	'nvim-treesitter/playground',
+	'theprimeagen/harpoon',
+	'mbbill/undotree',
+	'tpope/vim-fugitive',
+	'eandrju/cellular-automaton.nvim' 
 }
-
