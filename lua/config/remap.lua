6f1a08fc6490c343b0a808bd95443aebce448395
Kliprmimo <grzes.dziechciarz@gmail.com>
2024-07-19 00:26:46 +0200

    refactor

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,8 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+
 -- Keybinding to toggle inline diagnostics (ii)
-vim.api.nvim_set_keymap('n', '<Leader>ii', ':lua vim.cmd("DiagnosticsToggleVirtualText")<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ii', ':lua vim.cmd("DiagnosticsToggleVirtualText")<CR>',
+	{ noremap = true, silent = true })
 
 -- Keybinding to toggle diagnostics (id)
 vim.api.nvim_set_keymap('n', '<Leader>id', ':lua vim.cmd("DiagnosticsToggle")<CR>', { noremap = true, silent = true })
